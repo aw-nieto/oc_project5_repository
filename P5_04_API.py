f044151076dc555512807d3c55cbdd782efb7a68
aw-nieto <refalo.baptiste@outlook.com>
2021-03-25 09:44:20 +0100

    Update P5_04_API.py

--- P5_04_API.py
+++ P5_04_API.py
@@ -4,8 +4,8 @@ Created on Thu Mar  4 16:14:28 2021
 
 @author: AzertWay
 """
-from flask import Flask
-from flask_restful import Resource, Api, reqparse
+from flask import Flask, jsonify
+from flask_restful import Api, reqparse
 import os
 from P5_03_final import classification
 
@@ -13,23 +13,27 @@ app = Flask(__name__)
 api = Api(app)
 
 
-class Classification(Resource):
-    def post(self):
-        parser = reqparse.RequestParser()  # initialize
-        parser.add_argument('Title', required=True)
-        parser.add_argument('Body', required=True)
-        args = parser.parse_args()  # parse arguments to dictionary
-    
-        data = classification(args)
-            
-        return {'message': data[0]}, data[1]
+@app.route('/label/', methods=['POST'])
+def post():
+    parser = reqparse.RequestParser()  # initialize
+    parser.add_argument('Title', required=True)
+    parser.add_argument('Body', required=True)
+    args = parser.parse_args()  # parse arguments to dictionary
 
+    data = classification(args)
+
+    return jsonify({
+        'Message': data[0],
+        'METHOD': 'POST'
+    }), data[1]
+
+@app.route('/')
+def index():
+    return '<h1>OpenClassrooms - Projet 5 - Catégorisez des questions</h1>'
 
 def get_port():
   return int(os.environ.get("PORT", 5000))
 
 
-api.add_resource(Classification, '/')
-
 if __name__ == "__main__":
-    app.run(debug=False, port=get_port(), host='127.0.0.1')
+    app.run(debug=False, port=get_port())
